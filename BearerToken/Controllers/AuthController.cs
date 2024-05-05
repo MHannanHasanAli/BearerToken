5add9b699bccda9c7bf0c3fcfaf06961c5b225a5
Muhammad Hannan Hasan Ali <muhammadhannanhasanali@outlook.com>
2024-05-05 21:32:53 +0500

    Add project files.

---
+++ BearerToken/Controllers/AuthController.cs
@@ -0,0 +1,121 @@
+﻿using BearerToken.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BearerToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto UserRequest)
+        {
+            string passHash = BCrypt.Net.BCrypt.HashPassword(UserRequest.Password);
+
+            user.Username = UserRequest.Username;
+            user.PasswordHash = passHash;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto UserRequest)
+        {
+            if (user.Username != UserRequest.Username)
+            {
+                return BadRequest("User Not Found");
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(UserRequest.Password, user.PasswordHash))
+            {
+                return BadRequest("Wrong Password");
+            }
+
+            string token = CreateToken(user);
+            var refreshToken = CreateRefreshToken();
+            SetRefreshToken(refreshToken);
+
+            return Ok(token);
+        }
+
+        private void SetRefreshToken(RefreshToken newRefreshToken)
+        {
+            var cookieoptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Expires = newRefreshToken.Expires
+            };
+
+            Response.Cookies.Append("refreshToken", newRefreshToken.Token, cookieoptions);
+
+            user.RefreshToken = newRefreshToken.Token;
+            user.TokenCreated = newRefreshToken.Created;
+            user.TokenExpires = newRefreshToken.Expires;
+        }
+
+        [HttpPost("Refresh-Token")]
+        public async Task<ActionResult<string>> RefreshToken()
+        {
+            var refreshToken = Request.Cookies["refreshToken"];
+
+            if (!user.RefreshToken.Equals(refreshToken))
+            {
+                return Unauthorized("Invalid Refresh Token");
+            }
+            else if (user.TokenExpires < DateTime.Now)
+            {
+                return Unauthorized("Token Expired");
+            }
+
+            string token = CreateToken(user);
+            var newRefreshToken = CreateRefreshToken();
+            SetRefreshToken(newRefreshToken);
+
+            return Ok(token);
+        }
+        private RefreshToken CreateRefreshToken()
+        {
+            var refreshToken = new RefreshToken()
+            {
+                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+                Expires = DateTime.Now.AddMinutes(5)
+            };
+
+            return refreshToken;
+        }
+        private string CreateToken(User UserRequest)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(2),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+    }
+}
---
+++ BearerToken/Model/RefreshToken.cs
@@ -0,0 +1,9 @@
+﻿namespace BearerToken.Model
+{
+    public class RefreshToken
+    {
+        public required string Token { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Expires { get; set; }
+    }
+}
